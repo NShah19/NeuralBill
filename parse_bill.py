f2232b00cb703914d1c5c5066a8f98fa748bf75f
Terrence Ho <terrenceho.books@gmail.com>
2017-04-01 15:47:34 -0700

    Added functions to retrieve date and name of bill

--- parse_bill.py
+++ parse_bill.py
@@ -6,3 +6,17 @@ def get_bill_text(url):
     soup = BeautifulSoup(r.content, 'html.parser')
     text = soup.find(id = "billTextContainer").get_text()
     return text
+
+def get_bill_date(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    date = soup.find("h1", {"class":"legDetail"}).find('span').get_text()
+    return date
+
+def get_bill_name(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    whole_title = soup.find("h1", {"class":"legDetail"}).get_text()
+    date = soup.find("h1", {"class":"legDetail"}).find('span').get_text()
+    title = whole_title.replace("date", '')
+    return title
