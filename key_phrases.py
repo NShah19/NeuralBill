b1be53dafe6d0cff2b963397d46e99faef5abf9b
Terrence Ho <terrenceho.books@gmail.com>
2017-04-01 19:43:08 -0700

    Finished making my key_phrases

--- key_phrases.py
+++ key_phrases.py
@@ -3,10 +3,11 @@ import urllib.request, urllib.error, urllib.parse
 import sys
 import base64
 import json
+import csv
 from time import sleep
 from random import shuffle
 from api_keys import account_key
-from parse_bill import get_bill_text
+from parse_bill import get_bill_text, get_bill_start_date, get_bill_end_date, get_bill_name
 import operator
 
 # Azure portal URL.
@@ -52,6 +53,68 @@ for i in range(0, 95):
 sortedKeywords = sorted(keyword_dict.items(), key = operator.itemgetter(1))
 finalKeywordList = [sortedKeywords[(-1 * i) - 1][0] for i in range(min(500, len(sortedKeywords)))]
 
+bill_name = get_bill_name(url)
+bill_start_date = get_bill_start_date(url)
+bill_end_date = get_bill_end_date(url)
 
-# place into a set
+csvFile = open(bill_name + '.csv', 'w')
+writer = csv.writer(csvFile)
+writer.writerow([bill_name])
+writer.writerow([bill_start_date])
+writer.writerow([bill_end_date])
+for i in range(len(finalKeywordList)):
+    writer.writerow([finalKeywordList[i]])
+csvFile.close()
+
+    # place into a set
 print("Done")
+
+# def get_key_phrases(url):
+#     full_text = get_bill_text(url)
+#     list_full_text = full_text.split()
+#     result_list = [] # result list to store each 50000 new string
+#     maxwords = 500
+#     for i in range(0, (len(list_full_text)//maxwords) + 1):
+#         result_list.append(' '.join(list_full_text[i*maxwords:min((i+1) * maxwords, len(list_full_text))]))
+
+#     shuffle(result_list)
+#     input_texts = []
+#     for i in range(0, len(result_list)):
+#         input_texts.append('{"documents":[{"id":"%s","text":"%s"},]}' % (str(i+1), result_list[i]))
+#     print(len(input_texts))
+
+#     # Detect key phrases.
+#     batch_keyphrase_url = base_url + 'text/analytics/v2.0/keyPhrases'
+#     keyword_dict = {}
+#     for i in range(0, 95):
+#         print(i)
+#         req = urllib.request.Request(batch_keyphrase_url, input_texts[i].encode('utf-8'), headers)
+#         response = urllib.request.urlopen(req)
+#         result = response.read().decode('utf-8')
+#         obj = json.loads(result)
+#         keyphrase_analysis = obj['documents'][0]['keyPhrases']
+#         for j in range(len(keyphrase_analysis)):
+#             target = keyword_dict.get(keyphrase_analysis[j])
+#             if not target:
+#                 keyword_dict[keyphrase_analysis[j]] = 1
+#             else:
+#                 keyword_dict[keyphrase_analysis[j]] += 1
+#     #         print(('Key phrases ' + str(keyphrase_analysis['id']) + ': ' + ', '.join(map(str,keyphrase_analysis['keyPhrases']))))
+#     sortedKeywords = sorted(keyword_dict.items(), key = operator.itemgetter(1))
+#     finalKeywordList = [sortedKeywords[(-1 * i) - 1][0] for i in range(min(500, len(sortedKeywords)))]
+
+#     bill_name = get_bill_name(url)
+#     bill_start_date = get_bill_start_date(url)
+#     bill_end_date = get_bill_end_date(url)
+
+#     csvFile = open(bill_name + '.csv', 'w')
+#     writer = csv.writer(csvFile)
+#     writer.writerows([bill_name])
+#     writer.writerows([bill_start_date])
+#     writer.writerows([bill_end_date])
+#     for i in range(len(finalKeywordList)):
+#         writer.writerows(finalKeywordList[i])
+#     csvFile.close()
+
+#     # place into a set
+#     print("Done")
--- parse_bill.py
+++ parse_bill.py
@@ -7,16 +7,22 @@ def get_bill_text(url):
     text = soup.find(id = "billTextContainer").get_text()
     return text
 
-def get_bill_date(url):
+def get_bill_start_date(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    date = soup.find("h1", {"class":"legDetail"}).find('span').get_text()
-    return date
+    start_date = soup.find("div", {"class":"overview"}).find('td').get_text().split("Introduced ")[1][0:-1]
+    return start_date
+
+def get_bill_end_date(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    end_date = soup.find("div", {"class":"overview"}).find_all('tr')[-1].find('td').get_text().split()[0]
+    return end_date
 
 def get_bill_name(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     whole_title = soup.find("h1", {"class":"legDetail"}).get_text()
     date = soup.find("h1", {"class":"legDetail"}).find('span').get_text()
-    title = whole_title.replace("date", '')
+    title = whole_title.replace(date, '')
     return title
